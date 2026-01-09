8b2fa8ac42d5010ace738fa7010a2703ad4879e3
Niki <jefim.nik@gmail.com>
2026-01-09 15:41:54 +0100

    refactoring

--- collect_bom_type1_excel.py
+++ collect_bom_type1_excel.py
@@ -5,9 +5,9 @@ import os
 import pandas as pd
 
 def get_file_list(root_dir: str) ->list:
-    '''
+    """
     get all paths+filenames from dir folder and make upper case
-    '''
+    """
     file_list = []
     for folder_path, _, file_names in os.walk(root_dir):
         for file_name in file_names:
@@ -16,9 +16,9 @@ def get_file_list(root_dir: str) ->list:
     return file_list
 
 def filter_by_folder_and_filename(file_list: list, include_file, include_dir, exclude_file, exclude_dir) -> list:
-    '''
+    """
     exclude old and archived folders from list, files with specific names / include folders and files by rule
-    '''
+    """
     include_file = [x.upper() for x in include_file]
     include_dir = [x.upper() for x in include_dir]
     exclude_file = [x.upper() for x in exclude_file]
@@ -30,11 +30,15 @@ def filter_by_folder_and_filename(file_list: list, include_file, include_dir, ex
     file_list = [x for x in file_list if not include_file or any(z in os.path.basename(x) for z in include_file)]
     return file_list
 
-def remove_duplicates_by_filename(all_files: list) -> tuple[list, list]:
+def remove_duplicates_by_filename(file_list: list) -> tuple[list, list]:
+    """
+    find same file names and remove last paths from list
+    return filtered and dropped paths lists
+    """
     seen = []
     unique_files = []
     duplicates = []
-    for path in all_files:
+    for path in file_list:
         file_name = os.path.basename(path)
         if file_name not in seen:
             seen.append(file_name)
@@ -43,69 +47,83 @@ def remove_duplicates_by_filename(all_files: list) -> tuple[list, list]:
             duplicates.append(path)
     return unique_files, duplicates
 
-def filter_by_sheet_names(all_files: list) -> tuple:
+def filter_by_sheet_names(file_list: list) -> tuple:
     edms_bom_standard_list = []
     not_bom_file_list = []
+    error_file_list = []
     sheet_names = ["Summary", "BOM", "Error", "Warning", "Status"]
-
-    for path in all_files:
+    for path in file_list:
         try:
             xls = pd.ExcelFile(path, engine="xlrd")
             if all(i in xls.sheet_names for i in sheet_names):
                 edms_bom_standard_list.append(path)
+            else:
+                not_bom_file_list.append(path)
         except:
-            not_bom_file_list.append(path)
+            error_file_list.append(path)
 
-    return edms_bom_standard_list, not_bom_file_list
-
-
-
+    return edms_bom_standard_list, not_bom_file_list, error_file_list
 
 
 if __name__=="__main__":
     root_dir = r"C:\Users\user\Vilesco\DATA_CORE - Документы\Z34\Z34 CDA HVAC MFZ 1-2\RAW\TUY\EDMS\TUSNA"
-
     include_file = ["xls"]
     exclude_file = []
-
     include_dir = []
     exclude_dir = ["_archive"]
-
-
     path_corrections_bom = ["BOM", "RPT", "BUM", "BOB", "RTP"] #
 
-    all_files = get_file_list(root_dir)
-    print(f"LEN all_files: {len(all_files)}")
 
-    all_files = filter_by_folder_and_filename(all_files, include_file, exclude_file, include_dir, exclude_dir)
-    print(f"LEN all_files after include/ex: {len(all_files)}")
-    for i in all_files:
-        print(i)
+    file_list = get_file_list(root_dir)
+    print("get_file_list")
+    print(f"LEN file_list: {len(file_list)}\n")
+    # for i in file_list:
+    #     print(i)
 
-    # all_files, duplicates = remove_duplicates_by_filename(all_files)
-    # print(f"LEN all_files after remove duplicates in file names: {len(all_files)}")
-    # if duplicates:
-    #     print("Found duplicates")
-    #     for i in duplicates:
-    #         print(i)
-    # else:
-    #     print("No duplicates")
-    #
-    # print("")
-    #
-    # all_files,not_bom_file_list = filter_by_sheet_names(all_files)
-    # print(f"filter_edms_bom_files_by_sheets LEN: {len(all_files)}")
-    #
-    # if all_files:
-    #     print("all_files")
-    #     for i in all_files:
-    #         print(i)
-    # else:
-    #     print("No all_files")
-    #
-    # if not_bom_file_list:
-    #     print("not_bom_file_list")
-    #     for i in not_bom_file_list:
-    #         print(i)
-    # else:
-    #     print("No not_bom_file_list")
+    filter_by_folder_and_filename = filter_by_folder_and_filename(file_list, include_file, exclude_file, include_dir, exclude_dir)
+    print("filter_by_folder_and_filename")
+    print(f"LEN file_list: {len(filter_by_folder_and_filename)}\n")
+    # for i in filter_by_folder_and_filename:
+    #     print(i)
+
+    remove_duplicates_by_filename, list_of_duplicates = remove_duplicates_by_filename(filter_by_folder_and_filename)
+    print("remove_duplicates_by_filename")
+    print(f"LEN file_list: {len(remove_duplicates_by_filename)}")
+    if list_of_duplicates:
+        print("Found duplicates")
+        for i in list_of_duplicates:
+            print(i)
+    else:
+        print("No duplicates")
+    print("")
+
+
+
+
+
+    bom_file_list,not_bom_file_list,error_file_list = filter_by_sheet_names(remove_duplicates_by_filename)
+    print("\nfilter_by_sheet_names")
+    print(f"LEN file_list: {len(bom_file_list)}")
+    print(f"LEN not bom list: {len(not_bom_file_list)}")
+    print(f"LEN opening errors: {len(error_file_list)}")
+
+    if bom_file_list:
+        print("\nFile_list")
+        for i in bom_file_list:
+            print(i)
+    else:
+        print("\nNo files to display")
+
+    if not_bom_file_list:
+        print("\nWrong type of BOM (by sheets) or another Excel files")
+        for i in not_bom_file_list:
+            print(i)
+    else:
+        print("\nAll BOM ok")
+
+    if error_file_list:
+        print("\nError in opening file")
+        for i in error_file_list:
+            print(i)
+    else:
+        print("\nNo Opening Errors")
