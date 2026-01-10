fad41fa4662bb47f71592038d0516b39e66dab76
jniki <jefim.nik@gmail.com>
2026-01-10 22:56:57 +0100

    almost done

--- RawData.py
+++ RawData.py
@@ -1,30 +1,37 @@
+# python -m pip install --upgrade pip setuptools wheel
+# pip install xlrd
+# pip install openpyxl
+
 from config import root_dir, output_dir
 import os
 import pandas as pd
+import yaml
 
 
 class RawData:
-    def __init__(self, root_dir, output_dir,
-                 include_file=None,
-                 exclude_file=None,
-                 include_dir=None,
-                 exclude_dir=None,
-                 sheet_names=None):
+    def __init__(self, config):
 
-        self.root_dir = root_dir
-        self.output_dir = output_dir
+
+        cfg = config["collect_data"]
+
+        self.root_dir = config["path_data"]["root_dir"]
+        self.output_dir = config["path_data"]["output_dir"]
 
         self.file_list = []
-
-        self.include_file = include_file or "xls"  # for subclass
-        self.exclude_file = exclude_file or []  # for subclass
-
-        self.include_dir = include_dir or []
-        self.exclude_dir = exclude_dir or ["_archive"]
-
-        self.sheet_names = sheet_names or ["Summary", "BOM", "Error", "Warning", "Status"]  # for subclass
         self.error_file_list = []
         self.not_bom_file_list = []
+        self.df_combined = pd.DataFrame()
+        self.output_path = None
+
+        self.file_type = config["collect_data"]["file_type"]
+        self.include_file = config["collect_data"]["include_file"]
+        self.exclude_file = config["collect_data"]["exclude_file"]
+        self.include_dir = config["collect_data"]["include_dir"]
+        self.exclude_dir = config["collect_data"]["exclude_dir"]
+        self.sheet_names = config["collect_data"]["sheet_names"]
+        self.sheet_name_to_concat = config["collect_data"]["sheet_name_to_concat"]
+        self.header_row = config["collect_data"]["header_row"]
+        self.engine = config["collect_data"]["engine"]
 
         pd.set_option("display.max_columns", 8)
         pd.set_option("display.max_rows", 10)
@@ -110,7 +117,7 @@ class RawData:
         self.error_file_list = error_file_list
         return edms_bom_standard_list, not_bom_file_list, error_file_list
 
-    def print_list(self, file_list, text):
+    def print_list(self, file_list, text=""):
         print(f"\n---{text}---")
         if file_list:
             print(f"LEN list: {len(file_list)}")
@@ -119,19 +126,39 @@ class RawData:
         else:
             print("Empty list")
 
+    def make_df_from_excel_files(self):
+        """
+        collect and combine tables from approved BOM files from BOM sheet, headers on row 5 in Excel
+        """
+        df_combined = []
+        for i in self.file_list:
+            df = pd.read_excel(i, sheet_name=self.sheet_name_to_concat, header=self.header_row, engine=self.engine)
+            df_combined.append(df)
+        df_combined = pd.concat(df_combined, ignore_index=True)
+        self.df_combined = df_combined
+        return df_combined
 
-class RawDataBOMType1(RawData):
-    def __init__(self, root_dir, output_dir):
-        super().__init__(root_dir, output_dir)
-
-        self.include_file = ["xls"]
-        self.exclude_file = []
-
-        self.sheet_names = ["Summary", "BOM", "Error", "Warning", "Status"]
+    def df_to_excel(self):
+        if not self.df_combined.empty:
+            output_path = rf"{self.output_dir}\{self.file_type}_excel_collected.xlsx"
+            self.df_combined.to_excel(output_path, index=False)
+            self.output_path = output_path
+            return output_path
+        else:
+            print("\nEmpty df_combined, no export to Excel")
+            return None
 
 
 if __name__ == "__main__":
-    data = RawData(root_dir, output_dir)
+    with open("_config_bom_type1.yaml") as f:
+        config = yaml.safe_load(f)
+
+    print("--CONFIG--")
+    for i in config.items():
+        print(i)
+    print("")
+
+    data = RawData(config)
 
     print(f"Root: {data.root_dir}")
     print(f"Output: {data.output_dir}")
@@ -152,3 +179,8 @@ if __name__ == "__main__":
     data.print_list(data.file_list, "filter_by_sheet_names")
     data.print_list(data.not_bom_file_list, "not_bom_file_list")
     data.print_list(data.error_file_list, "error_file_list")
+
+    data.make_df_from_excel_files()
+    print(f"\nDF shape: {data.df_combined.shape}")
+    data.df_to_excel()
+    print(f"\nExcel saved to: {data.output_path}")
