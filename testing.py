15ed2e2ef5ac66532c493a720a1887ddc238548e
jniki <jefim.nik@gmail.com>
2026-01-10 18:36:30 +0100

    get_file_list working

--- testing.py
+++ testing.py
@@ -1,13 +1,69 @@
+from config import root_dir, output_dir
 import os
 import pandas as pd
 
+class RawData:
+    def __init__(self, root_dir, output_dir):
+        self.root_dir = root_dir
+        self.output_dir = output_dir
 
-# pip install --upgrade pandas==1.5.3 --prefer-binary
-# pip install xlrd==1.2.0
-# python -m pip install --upgrade pip setuptools wheel
+        self.file_list = []
 
-path = r"C:\USERS\USER\VILESCO\DATA_CORE - ДОКУМЕНТЫ\Z34\Z34 CDA HVAC MFZ 1-2\RAW\TUY\EDMS\TUSNA\DOC0000231069,TUSNA441,E4\BOM_TUSNA441_E4.XLS"
-df = pd.read_excel(path, sheet_name="BOM", header=3, engine="xlrd", dtype=str)
+        self.include_file = ["xls"]
+        self.exclude_file = []
+        self.include_dir = []
+        self.exclude_dir = ["_archive"]
 
-test1 = pd.ExcelFile(path, engine="xlrd")
-print(test1.sheet_names)
+        pd.set_option("display.max_columns", 8)
+        pd.set_option("display.max_rows", 10)
+        pd.set_option("display.max_colwidth", 8)
+        pd.set_option("display.width", 150)
+        pd.set_option("display.expand_frame_repr", False)
+
+    def get_file_list(self):
+        """
+        get all paths+filenames from dir folder and make upper case
+        """
+        temp_file_list = []
+        for folder_path, _, file_names in os.walk(self.root_dir):
+            for file_name in file_names:
+                full_path = os.path.join(folder_path, file_name).upper()
+                temp_file_list.append(full_path)
+        self.file_list = temp_file_list
+        return temp_file_list
+
+    def filter_by_folder_and_filename(self) -> list:
+        """
+        exclude old and archived folders from list, files with specific names / include folders and files by rule
+        """
+        include_file = [x.upper() for x in self.include_file]
+        include_dir = [x.upper() for x in self.include_dir]
+        exclude_file = [x.upper() for x in self.exclude_file]
+        exclude_dir = [x.upper() for x in self.exclude_dir]
+
+        file_list = [x for x in file_list if not exclude_dir or not any(z in os.path.dirname(x) for z in exclude_dir)]
+        file_list = [x for x in file_list if not include_dir or any(z in os.path.dirname(x) for z in include_dir)]
+        file_list = [x for x in file_list if
+                     not exclude_file or not any(z in os.path.basename(x) for z in exclude_file)]
+        file_list = [x for x in file_list if not include_file or any(z in os.path.basename(x) for z in include_file)]
+        return file_list
+
+    def remove_duplicates_by_filename(self) -> tuple[list, list]:
+        """
+        find same file names and remove last paths from list
+        return filtered and dropped paths lists
+        """
+        seen = []
+        unique_files = []
+        duplicates = []
+        for path in file_list:
+            file_name = os.path.basename(path)
+            if file_name not in seen:
+                seen.append(file_name)
+                unique_files.append(path)
+            elif file_name in seen:
+                duplicates.append(path)
+        return unique_files, duplicates
+class RawDataBOMType1(RawData):
+    def __init__(self, root_dir, output_dir):
+        super().__init__(root_dir, output_dir)
