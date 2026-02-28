18d26e73031192fba322057ff0760084cfea70f7
jniki <jefim.nik@gmail.com>
2026-02-28 18:00:00 +0100

    run_01.py working in test mode

--- project_data_app/run_01.py
+++ project_data_app/run_01.py
@@ -16,31 +16,21 @@ if __name__ == "__main__":
 
     # pipeline to transform and  filter file list
     file_list = source.get_files()
-    print(len(file_list))
 
-    df_01 = files.os_file_paths_to_df(file_list)
-    logger.update_log_df(df_01, "01")
+    df = files.os_file_paths_to_df(file_list)
+    logger.update_log_df(df, "01")
 
-    df_02 = files.filter_by_folder(df_01)
-    logger.update_log_df(df_02, "02", ["dir"])
+    steps = [
+        (files.filter_by_folder, ["dir"], "02"),
+        (files.filter_by_extension, ["filename"], "03"),
+        (files.filter_by_filename, ["filename"], "04"),
+        (files.remove_duplicates_by_filename, ["filename"], "05"),
+        (files.filter_by_sheet_names, ["filename"], "06"),
+    ]
 
-    df_03 = files.filter_by_extension(df_02)
-    logger.update_log_df(df_03, "025", ["filename"])
-
-    df_04 = files.filter_by_filename(df_03)
-    logger.update_log_df(df_04, "03", ["filename"])
-
-    df_05 = files.remove_duplicates_by_filename(df_04)
-    logger.update_log_df(df_05, "04", ["filename"])
-
-    df_06 = files.filter_by_sheet_names(df_05)
-    logger.update_log_df(df_06, "05", ["filename"])
-
-    print(f"os_file_paths_to_df {df_01.shape[0]}")
-    print(f"filter_by_folder {df_02.shape[0]}")
-    print(f"filter_by_extension {df_03.shape[0]}")
-    print(f"filter_by_filename {df_04.shape[0]}")
-    print(f"remove_duplicates_by_filename {df_05.shape[0]}")
-    print(f"filter_by_sheet_names {df_06.shape[0]}")
+    for func, cols, step_name in steps:
+        df = func(df)
+        logger.update_log_df(df, step_name, cols)
+        print(f"{func.__name__}: {df.shape[0]}")
 
     files.df_to_excel(logger.df, "steps")
