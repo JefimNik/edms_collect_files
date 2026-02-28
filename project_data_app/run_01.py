0de679550aaae14cd677ed9cb78ab28ec1f08a6e
jniki <jefim.nik@gmail.com>
2026-02-28 19:31:32 +0100

    renaming

--- project_data_app/run_01.py
+++ project_data_app/run_01.py
@@ -1,40 +1,18 @@
-from services.FilesystemService import FilesystemService
-from sources.LocalfilesSource import LocalFileSource
-from services.ConfigManager import ConfigManager
-from services.StepLoggerService import StepLogger
-from pipelines.paths_pipeline import PathsPipeline
+from operations.Filesystem import FilesystemService
+from operations.Dataframe import LocalFileSource
+from operations.Config import ConfigManager
+from services.steplogger_service import StepLogger
+from services.paths_service import PathsService
 
 if __name__ == "__main__":
+
     # -------- INIT --------
     config = ConfigManager("config_01", "xls")
     source = LocalFileSource(config)
     files = FilesystemService(config)
     logger = StepLogger()
 
-    paths_pipeline = PathsPipeline(source, logger, files)
-
-    # -------- COLLECT --------
-    file_list = source.get_files()
-    df_paths = files.os_file_paths_to_df(file_list)
-    logger.update_log_df(df_paths, "01")
-
-    # -------- FILTER FILE PATHS --------
-    steps = [
-        (files.filter_by_folder, ["dir"], "02"),
-        (files.filter_by_extension, ["filename"], "03"),
-        (files.filter_by_filename, ["filename"], "04"),
-        (files.remove_duplicates_by_filename, ["filename"], "05"),
-        (files.filter_by_sheet_names, ["filename"], "06"),
-    ]
-
-    for func, cols, step_name in steps:
-        df_paths = func(df_paths)
-        logger.update_log_df(df_paths, step_name, cols)
-        print(f"{step_name} {func.__name__}: {df_paths.shape[0]}")
-
-    # -------- RESULT --------
-    filtered_file_paths = df_paths["filepath"].tolist()
-    files.df_to_excel(logger.df, "steps")
-
-
+    # -------- RUN --------
+    s01_paths = PathsService(source, logger, files)
+    s01_paths.run()
 
--- project_data_app/services/paths_service.py
+++ project_data_app/services/paths_service.py
@@ -1,14 +1,16 @@
-class PathsPipeline:
+class PathsService:
     def __init__(self, source, logger, files):
         self.source = source
         self.files = files
         self.logger = logger
 
     def run(self):
+        print("\n<<PathsService start>>")
         # -------- GET FILE PATHS --------
         file_list = self.source.get_files()
         df_paths = self.files.os_file_paths_to_df(file_list)
         self.logger.update_log_df(df_paths, "01")
+        print(f"01 os_file_paths: {len(df_paths)}")
 
         # -------- FILTER FILE PATHS --------
         steps = [
@@ -22,10 +24,11 @@ class PathsPipeline:
         for func, cols, step_name in steps:
             df_paths = func(df_paths)
             self.logger.update_log_df(df_paths, step_name, cols)
-            # print(f"{step_name} {func.__name__}: {df_paths.shape[0]}")
+            print(f"{step_name} {func.__name__}: {df_paths.shape[0]}")
+
+        self.files.df_to_excel(self.logger.df, "s01_paths")
+        print(f"\ns01_paths.xlsx created: {self.files.output_dir}")
+
+        print("<<PathsService finished>>")
 
         return df_paths["filepath"].tolist()
-
-
-if __name__ == "__main__":
-    pass
