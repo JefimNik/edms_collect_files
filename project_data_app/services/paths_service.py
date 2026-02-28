e97556bdd66378a86e37367ee5c6d2b42d230751
jniki <jefim.nik@gmail.com>
2026-02-28 21:47:21 +0100

    bl working

--- project_data_app/services/paths_service.py
+++ project_data_app/services/paths_service.py
@@ -1,24 +1,24 @@
 class PathsService:
     def __init__(self, files, dfs, logger):
-        self.source = files
-        self.files = dfs
+        self.files = files
+        self.dfs = dfs
         self.logger = logger
 
     def run(self):
         print("\n<<PathsService start>>")
         # -------- GET FILE PATHS --------
-        file_list = self.source.get_files()
-        df_paths = self.files.os_file_paths_to_df(file_list)
+        file_list = self.files.get_files()
+        df_paths = self.dfs.os_file_paths_to_df(file_list)
         self.logger.update_log_df(df_paths, "01")
         print(f"01 os_file_paths: {len(df_paths)}")
 
         # -------- FILTER FILE PATHS --------
         steps = [
-            (self.files.filter_by_folder, ["dir"], "02"),
-            (self.files.filter_by_extension, ["filename"], "03"),
-            (self.files.filter_by_filename, ["filename"], "04"),
-            (self.files.remove_duplicates_by_filename, ["filename"], "05"),
-            (self.files.filter_by_sheet_names, ["filename"], "06"),
+            (self.dfs.filter_by_folder, ["dir"], "02"),
+            (self.dfs.filter_by_extension, ["filename"], "03"),
+            (self.dfs.filter_by_filename, ["filename"], "04"),
+            (self.dfs.remove_duplicates_by_filename, ["filename"], "05"),
+            (self.dfs.filter_by_sheet_names, ["filename"], "06"),
         ]
 
         for func, cols, step_name in steps:
@@ -26,8 +26,8 @@ class PathsService:
             self.logger.update_log_df(df_paths, step_name, cols)
             print(f"{step_name} {func.__name__}: {df_paths.shape[0]}")
 
-        self.files.df_to_excel(self.logger.df, "s01_paths")
-        print(f"\ns01_paths.xlsx created: {self.files.output_dir}")
+        self.dfs.df_to_excel(self.logger.df, "s01_paths")
+        print(f"\ns01_paths.xlsx created: {self.dfs.output_dir}")
 
         print("<<PathsService finished>>")
 
